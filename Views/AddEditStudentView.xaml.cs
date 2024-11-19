85fa0fc6ed7e7ae9259f111c981fb70188f110b1
KamilRzepka-kml <rzepekvp93@gmail.com>
2024-11-19 19:42:48 +0100

    Property changed 5:00

--- Views/AddEditStudentView.xaml.cs
+++ Views/AddEditStudentView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace StudentDiaryWPF
+namespace StudentDiaryWPF.Views
 {
     /// <summary>
     /// Interaction logic for AddEditStudentView.xaml
