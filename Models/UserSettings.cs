34f4ce207fcd551f64627d979ba09e419bc7623b
KML <rzepekvp93@gmail.com>
2025-03-01 20:30:29 +0100

    Properties ok 14

---
+++ Models/UserSettings.cs
@@ -0,0 +1,69 @@
+﻿using StudentDiaryWPF.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiaryWPF.Models
+{
+    public class UserSettings
+    {
+        public string ServerAdress 
+        { 
+            get
+            {
+                return Settings.Default.ServerAdress;
+            } 
+            set
+            {
+                Settings.Default.ServerAdress = value;
+            }
+        }
+        public string ServerName
+        {
+            get
+            {
+                return Settings.Default.ServerName;
+            }
+            set
+            {
+                Settings.Default.ServerName = value;
+            }
+        }
+        public string Database
+        {
+            get
+            {
+                return Settings.Default.Database;
+            }
+            set
+            {
+                Settings.Default.Database = value;
+            }
+        }
+        public string User
+        {
+            get
+            {
+                return Settings.Default.User;
+            }
+            set
+            {
+                Settings.Default.User = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return Settings.Default.Password;
+            }
+            set
+            {
+                Settings.Default.Password = value;
+            }
+        }
+
+    }
+}
