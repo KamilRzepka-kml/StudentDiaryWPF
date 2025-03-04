c92f0365e2ff89cad444e53557c712b71f0cd043
KamilRzepka-kml <rzepekvp93@gmail.com>
2025-03-04 12:37:11 +0100

    UserSettingsa.cs 20

--- Models/UserSettings.cs
+++ Models/UserSettings.cs
@@ -1,20 +1,27 @@
 ﻿using StudentDiaryWPF.Properties;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudentDiaryWPF.Models
 {
-    public class UserSettings
+    public class UserSettings : IDataErrorInfo
     {
-        public string ServerAdress 
-        { 
+        public bool _isServerAdressValid;
+        public bool _isServerNameValid;
+        public bool _isDatabaseValid;
+        public bool _isUserValid;
+        public bool _isPasswordValid;
+
+        public string ServerAdress
+        {
             get
             {
                 return Settings.Default.ServerAdress;
-            } 
+            }
             set
             {
                 Settings.Default.ServerAdress = value;
@@ -65,5 +72,89 @@ namespace StudentDiaryWPF.Models
             }
         }
 
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(ServerAdress):
+                        if (string.IsNullOrWhiteSpace(ServerAdress))
+                        {
+                            Error = "Pole Adres serwera jest wymagane.";
+                            _isServerAdressValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerAdressValid = true;
+
+                        }
+                        break;
+                    case nameof(ServerAdress):
+                        if (string.IsNullOrWhiteSpace(ServerAdress))
+                        {
+                            Error = "Pole Adres serwera jest wymagane.";
+                            _isServerAdressValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerAdressValid = true;
+
+                        }
+                        break;
+                    case nameof(ServerAdress):
+                        if (string.IsNullOrWhiteSpace(ServerAdress))
+                        {
+                            Error = "Pole Adres serwera jest wymagane.";
+                            _isServerAdressValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerAdressValid = true;
+
+                        }
+                        break;
+                    case nameof(ServerAdress):
+                        if (string.IsNullOrWhiteSpace(ServerAdress))
+                        {
+                            Error = "Pole Adres serwera jest wymagane.";
+                            _isServerAdressValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerAdressValid = true;
+
+                        }
+                        break;
+                    case nameof(ServerAdress):
+                        if (string.IsNullOrWhiteSpace(ServerAdress))
+                        {
+                            Error = "Pole Adres serwera jest wymagane.";
+                            _isServerAdressValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerAdressValid = true;
+
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return Error;
+            }
+        }
+
+        public string Error { get;set;}
+
+        internal void Save()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿using StudentDiaryWPF.Commands;
+using StudentDiaryWPF.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace StudentDiaryWPF.ViewModels
 {
-    public class SettingsViewModel
+    public class SettingsViewModel : ViewModelBase
     {
+        private UserSettings _userSettings;
+
+        public SettingsViewModel()
+        {
+            CloseCommand = new RelayCommand(Close);
+            ConfirmCommand = new RelayCommand(Confirm);
+            _userSettings = new UserSettings();
+
+        }
+
+        public ICommand CloseCommand { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+
+        public UserSettings UserSettings 
+        { 
+            get
+            {
+                return _userSettings;
+            }
+            set
+            {
+                _userSettings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Confirm(object obj)
+        {
+            //walidacja
+            UserSettings.Save();
+            RestartApplication();
+        }
+
+        private void RestartApplication()
+        {
+            Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+        }
+
+        private void Close(object obj)
+        {
+            CloseWindow(obj as Window);
+        }
+
+        private void CloseWindow(Window window)
+        {
+            window.Close();
+        }
     }
 }
